b364975722f7c996607ee54fdfc71a752b053d5f
Илья <socspec@yandex.ru>
2022-10-26 22:19:32 +0300

    1

--- main.py
+++ main.py
@@ -17,8 +17,29 @@ def get_articles_urls(url):
         response = session.get(url=url, headers=headers, verify=False)
 
     soup = BeautifulSoup(response.text, 'lxml')
-    pagination_count = int(soup.find('ul', class_ = 'pagination').find_all('a')[-1].text)
-    print(pagination_count)
+    pagination_count = int(soup.find('ul', class_ = 'pagination').find_all('a', class_='page-link')[-2].text)
+
+
+    with requests.Session() as session:
+        for page in range(1, pagination_count + 1):
+            print(page)
+
+            response = session.get(url=f'https://neiros.ru/blog/analytics/?page={page}', headers=headers, verify=False)
+            
+            soup = BeautifulSoup(response.text, 'lxml')
+            articles_url = soup.find_all('a', class_='icon-right')
+
+
+
+            for au in articles_url:
+                art_url = 'https://neiros.ru' + au.get('href')
+                print(art_url)
+
+            
+
+
+
+
 
     # with open('data/index_2.html', 'w') as file:
     #     file.write(response.text)
